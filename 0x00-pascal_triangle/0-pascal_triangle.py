758641c93d32cda4355189da8ec3897ddd160309
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-08-10 12:21:32 +0200

    [Update] - Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module that has pascal_triangle method"""
+
+
+def pascal_triangle(n):
+    """Method for pascal's triangle"""
+    res = [[1]]
+
+    if (n <= 0):
+        return []
+
+    for i in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res) + 1):
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
+
+    return res
